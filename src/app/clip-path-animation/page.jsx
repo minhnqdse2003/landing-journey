55c0c3c036f4e81735d9ac8b96659f6333ad8c2c
minhnqdse2003 <minhnqdse2003@gmail.com>
2025-02-24 14:52:48 +0700

    init

---
+++ src/app/clip-path-animation/page.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./style.css";
+
+const Page = () => {
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <svg
+        width="70%"
+        height="70%"
+        viewBox="0 0 4588 3841"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M4588 380C4588 170.132 4417.87 0 4208 0H3039C2829.13 0 2659 170.132 2659 380V786C2659 1023.48 2466.48 1216 2229 1216H380C170.132 1216 0 1386.13 0 1596V3461C0 3670.87 170.132 3841 380 3841H4208C4417.87 3841 4588 3670.87 4588 3461V380Z"
+          fill="white"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const Navbar = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const router = useRouter();
+
+  const navigationItems = [
+    { name: "Clip Path Animation", path: "/clip-path-animation" },
+  ];
+
+  const handleNavigation = (path) => {
+    router.push(path);
+    setDropdownOpen(false); // Close dropdown after navigation
+  };
+
+  return (
+    <nav className="bg-blue-600 p-4 text-white flex justify-center items-center w-full">
+      <div className="relative">
+        <button
+          onClick={() => setDropdownOpen(!dropdownOpen)}
+          className="bg-blue-700 px-4 py-2 rounded hover:bg-blue-800"
+        >
+          Menu
+        </button>
+        {dropdownOpen && (
+          <div className="absolute right-[-50%] mt-2 w-48 bg-white text-black shadow-lg rounded max-h-[50vh] overflow-y-scroll">
+            {navigationItems.map((item) => (
+              <button
+                key={item.path}
+                onClick={() => handleNavigation(item.path)}
+                className="block w-full text-left px-4 py-2 hover:bg-gray-200"
+              >
+                {item.name}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
